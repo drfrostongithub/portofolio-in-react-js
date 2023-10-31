45e25ceacb00b41305a5fdb364d4bfa859cc83b4
drfrostongithub <eternal.putra@gmail.com>
2023-10-31 15:13:04 +0700

    delete framer and update react-script to latest

--- src/App.js
+++ src/App.js
@@ -1,44 +1,27 @@
-import React,{ Component } from 'react';
-import Header from './components/header/header';
-import About from './components/about/about';
-import Resume from './components/resume/resume';
-import Portofolio from './components/portofolio/portofolio';
-import Testimonials from './components/testimonials/testimonials';
-import ContacMe from './components/contacme/contacme';
-import Footer from './components/footer/footer';
-import {motion} from 'framer-motion' 
-import resumeData from './components/resume/resumeData'
+import React, { Component } from "react";
+import Header from "./components/header/header";
+import About from "./components/about/about";
+import Resume from "./components/resume/resume";
+import Portofolio from "./components/portofolio/portofolio";
+import Testimonials from "./components/testimonials/testimonials";
+import ContacMe from "./components/contacme/contacme";
+import Footer from "./components/footer/footer";
+import resumeData from "./components/resume/resumeData";
 
 class App extends Component {
-    render () {
-      return (
-        <motion.div className="App"
-          initial={{
-            opacity: 0,
-            x:100,
-            y:100
-          }}
-          animate={{
-            opacity:1,
-            x:0,
-            y:0
-          }}
-          transition={{
-            duration: 5
-          }}
-        >
-          <Header resumeData={resumeData} />
-          <About />
-          <Resume />
-          <Portofolio />
-          <Testimonials />
-          <ContacMe />
-          <Footer />
-        </motion.div>
+  render() {
+    return (
+      <div className='App'>
+        <Header resumeData={resumeData} />
+        <About />
+        <Resume />
+        <Portofolio />
+        <Testimonials />
+        <ContacMe />
+        <Footer />
+      </div>
     );
   }
-
-
 }
 
-export default App
+export default App;
